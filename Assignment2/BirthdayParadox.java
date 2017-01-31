6456778fb0ccb03e8d08d9993549475f5517b2fe
Tolu Olubode <toluwanimiolubode@gmail.com>
2017-01-31 18:37:12 -0500

    assignment 2 set up

---
+++ Assignment2/BirthdayParadox.java
@@ -0,0 +1,70 @@
+/**
+ * The class  <b>BirthdayParadox</b> is used to
+ * simulated the so-called Birthday paradox, and uses
+ * the class <b>Statistics</b> to store the results of
+ * the experiments.
+ *
+ * @author gvj (gvj@eecs.uottawa.ca)
+ *
+ */
+
+public class BirthdayParadox {
+
+
+	/**
+     * Random generator
+     */
+	private static java.util.Random generator = new java.util.Random();
+
+
+	/**
+     * Runs the series of experiments, and stores the result into
+     * a Statistics object
+     *
+     * @param range the size of the set from which random number are drawn
+     * @param numberOfRuns the number of experiments to run
+	 *
+	 * @return a reference to a Statistics instance that holds the result
+	 * of the experiment
+     */
+ 	public static Statistics runExperiments(int range, int numberOfRuns){
+
+// REPLACE THE BODY OF THIS METHOD WITH YOUR OWN IMPLEMENTATION
+
+	}
+
+ 	/**
+     * Runs a single experiment.
+     * The parameter range defines the size of the set from which
+     * the experiment is drawn
+     *
+     * @param range the size of the set from which random number are drawn
+     *
+	 * @return the number of random draw in the set that the method
+	 * used before drawing the same element for the second time
+     */
+
+ 	private static int oneRun(int range){
+
+// REPLACE THE BODY OF THIS METHOD WITH YOUR OWN IMPLEMENTATION
+
+	}
+
+
+	/**
+     * Main method. The default size of the set is 365, and
+     * the experiment is run 50 times. Both numbers can be reset
+     * from the command line.
+     * This method runs the experiments and prints the
+     * resulting Statistics
+     *
+     * @param args if not empty, contains the runtime values for
+     * the size of the set and the number of runs
+     */
+	public static void main(String[] args) {
+
+// REPLACE THE BODY OF THIS METHOD WITH YOUR OWN IMPLEMENTATION
+
+	}
+
+}
---
+++ Assignment2/Statistics.java
@@ -0,0 +1,78 @@
+
+/**
+ * The class  <b>Statistics</b> accumulates the results of
+ * the experiments. It know ahead of time how many experiments
+ * will be run, and provides at the end the min, the max, the
+ * average and the standard deviation for the data.
+ *
+ * <b> this class should not use classes such as Array,
+ * Lists etc. </b> to store the data, only prinitive types
+ * and java arrays.
+ *
+ * @author gvj (gvj@eecs.uottawa.ca)
+ *
+ */
+public class Statistics {
+
+// ADD HERE INSTANCE VARIABLES DECLARATION
+
+	/**
+     * Constructor.
+     *
+     * @param numberOfRuns the number of experiments that will be run
+     */
+ 	public  Statistics(int numberOfRuns){
+
+// REPLACE THE BODY OF THIS METHOD WITH YOUR OWN IMPLEMENTATION
+
+	}
+
+	/**
+     * Updates statistics after one experiment.
+     * This method cannot be called more times than the
+     * paramter that was passed in the constructor. If
+     * it is, an error message should be printed and
+     * no change should occur.
+     *   @param value the result of the new experiment
+     */
+	public void updateStatistics(int value){
+
+// REPLACE THE BODY OF THIS METHOD WITH YOUR OWN IMPLEMENTATION
+
+	}
+
+
+	/**
+     *   @return the current average of the values passed
+     * to the method updateStatistic
+     */
+	public double average(){
+
+// REPLACE THE BODY OF THIS METHOD WITH YOUR OWN IMPLEMENTATION
+
+	}
+
+
+	/**
+     *   @return the current standard deviation of the values passed
+     * to the method updateStatistic
+     */
+	public double standardDeviation(){
+
+// REPLACE THE BODY OF THIS METHOD WITH YOUR OWN IMPLEMENTATION
+
+	}
+
+	/**
+     *  @return Returns the complete statistics information:
+     * current minimum, current maximim, current average and
+     * current standard deviation. For the last two, only two
+     * digits decimals are shown
+     */
+	public String toString(){
+
+// REPLACE THE BODY OF THIS METHOD WITH YOUR OWN IMPLEMENTATION
+
+	}
+
+}
